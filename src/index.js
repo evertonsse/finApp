933c8e7a7b5d13d2b0a3ac16895e2542c37d771f
evertonsse <evertonsse@outlook.com>
2021-12-03 21:38:30 -0300

    desenvolvimento do estudo

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid')
 app.use(express.json());
 
+const customers = []
 
-/* 
-CPF - strin
-name - string
-id - uuid
-statement - []
-*/
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
 
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(404).json({ error: "Conta não cadastrada!" })
+    }
+    request.customer = customer;
+
+    return next();
+
+}
 
 app.post('/account', (request, response) => {
-    const {cpf, name} = request.body; 
-    
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(
+        customer => customer.cpf === cpf);
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "CPF já cadastrado!" });
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send();
+});
+
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    return response.json(customer.statement);
 })
 
 app.listen(3333);
